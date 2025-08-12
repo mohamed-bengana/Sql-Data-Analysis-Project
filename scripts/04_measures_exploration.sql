dfc76eed0de77f1e39dc045f2d2025173010f190
mohamed-bengana <bengana083@gmail.com>
2025-08-12 15:32:09 +0100

    Update 04_measures_exploration.sql

--- scripts/04_measures_exploration.sql
+++ scripts/04_measures_exploration.sql
@@ -2,13 +2,6 @@
 ===============================================================================
 Measures Exploration (Key Metrics)
 ===============================================================================
-Purpose:
-    - To calculate aggregated metrics (e.g., totals, averages) for quick insights.
-    - To identify overall trends or spot anomalies.
-
-SQL Functions Used:
-    - COUNT(), SUM(), AVG()
-===============================================================================
 */
 
 -- Find the Total Sales
