f255e133fb3bd46a40898d20ff1703c8a26becf4
mohamed-bengana <bengana083@gmail.com>
2025-08-12 15:34:11 +0100

    Update 07_change_over_time_analysis.sql

--- scripts/07_change_over_time_analysis.sql
+++ scripts/07_change_over_time_analysis.sql
@@ -2,15 +2,6 @@
 ===============================================================================
 Change Over Time Analysis
 ===============================================================================
-Purpose:
-    - To track trends, growth, and changes in key metrics over time.
-    - For time-series analysis and identifying seasonality.
-    - To measure growth or decline over specific periods.
-
-SQL Functions Used:
-    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
-    - Aggregate Functions: SUM(), COUNT(), AVG()
-===============================================================================
 */
 
 -- Analyse sales performance over time
@@ -47,3 +38,4 @@ FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY FORMAT(order_date, 'yyyy-MMM')
 ORDER BY FORMAT(order_date, 'yyyy-MMM');
+
