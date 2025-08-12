1c44dca7ff709fd740c2e8d93f7495264fe7f938
mohamed-bengana <bengana083@gmail.com>
2025-08-12 15:37:46 +0100

    Update 10_data_segmentation.sql

--- scripts/10_data_segmentation.sql
+++ scripts/10_data_segmentation.sql
@@ -2,14 +2,6 @@
 ===============================================================================
 Data Segmentation Analysis
 ===============================================================================
-Purpose:
-    - To group data into meaningful categories for targeted insights.
-    - For customer segmentation, product categorization, or regional analysis.
-
-SQL Functions Used:
-    - CASE: Defines custom segmentation logic.
-    - GROUP BY: Groups data into segments.
-===============================================================================
 */
 
 /*Segment products into cost ranges and 
@@ -66,4 +58,5 @@ FROM (
     FROM customer_spending
 ) AS segmented_customers
 GROUP BY customer_segment
+
 ORDER BY total_customers DESC;
