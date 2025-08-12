7f44a24f46d6080fcacfe2e34522bbec14813230
mohamed-bengana <bengana083@gmail.com>
2025-08-12 15:33:33 +0100

    Update 06_ranking_analysis.sql

--- scripts/06_ranking_analysis.sql
+++ scripts/06_ranking_analysis.sql
@@ -2,14 +2,6 @@
 ===============================================================================
 Ranking Analysis
 ===============================================================================
-Purpose:
-    - To rank items (e.g., products, customers) based on performance or other metrics.
-    - To identify top performers or laggards.
-
-SQL Functions Used:
-    - Window Ranking Functions: RANK(), DENSE_RANK(), ROW_NUMBER(), TOP
-    - Clauses: GROUP BY, ORDER BY
-===============================================================================
 */
 
 -- Which 5 products Generating the Highest Revenue?
@@ -22,20 +14,6 @@ LEFT JOIN gold.dim_products p
     ON p.product_key = f.product_key
 GROUP BY p.product_name
 ORDER BY total_revenue DESC;
-
--- Complex but Flexibly Ranking Using Window Functions
-SELECT *
-FROM (
-    SELECT
-        p.product_name,
-        SUM(f.sales_amount) AS total_revenue,
-        RANK() OVER (ORDER BY SUM(f.sales_amount) DESC) AS rank_products
-    FROM gold.fact_sales f
-    LEFT JOIN gold.dim_products p
-        ON p.product_key = f.product_key
-    GROUP BY p.product_name
-) AS ranked_products
-WHERE rank_products <= 5;
 
 -- What are the 5 worst-performing products in terms of sales?
 SELECT TOP 5
