7a6f73a7c5b888a98535938561379e037809a4db
mohamed-bengana <bengana083@gmail.com>
2025-08-12 15:32:42 +0100

    Update 05_magnitude_analysis.sql

--- scripts/05_magnitude_analysis.sql
+++ scripts/05_magnitude_analysis.sql
@@ -2,14 +2,6 @@
 ===============================================================================
 Magnitude Analysis
 ===============================================================================
-Purpose:
-    - To quantify data and group results by specific dimensions.
-    - For understanding data distribution across categories.
-
-SQL Functions Used:
-    - Aggregate Functions: SUM(), COUNT(), AVG()
-    - GROUP BY, ORDER BY
-===============================================================================
 */
 
 -- Find total customers by countries
